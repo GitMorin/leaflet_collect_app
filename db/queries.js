dcd6df59b4bf3745e370ed03b47c45ba7c6fc5a8
Kristian <kristian.morin@gmail.com>
2017-12-02 20:42:29 +0000

    Fixed linting errors and modified .gitignore

--- db/queries.js
+++ db/queries.js
@@ -8,14 +8,14 @@ const db = knex({
   dialect: 'postgres'
 });
 
-const allPois = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(ST_Transform(lg.geom,4326))::json As geometry , row_to_json((SELECT l FROM (SELECT id, place, comments, regdate, numbers) As l)) As properties FROM poi As lg   ) As f )  As fc;"
+const allPois = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(ST_Transform(lg.geom,4326))::json As geometry , row_to_json((SELECT l FROM (SELECT id, place, comments, regdate, numbers) As l)) As properties FROM poi As lg   ) As f )  As fc;";
 
 module.exports = {
   getAll() {
     // retrun all rows in pois table
-    return knex.raw(allPois)
+    return knex.raw(allPois);
   },
-  getOne(id){
+  getOne(id) {
     return knex('poi').where('id', id).first();
   },
 
@@ -25,7 +25,7 @@ module.exports = {
     const sql = db.insert({
       geom: st.geomFromText('Point(-71.064544 44.28787)', 4326)
     }).into('poi');
-    return sql
+    return sql;
   },
   update(id, poi) {
     return knex('poi').where('id', id).update(poi, '*');
