f51cd8ae8c7444d3e117b9bb44bd58fb22d4ed6e
Kristian <kristian.morin@gmail.com>
2018-02-24 18:10:09 +0000

    test

--- db/queries.js
+++ db/queries.js
@@ -8,7 +8,8 @@ const db = knex({
   dialect: 'postgres'
 });
 
-const allPois = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(ST_Transform(lg.geom,4326))::json As geometry , row_to_json((SELECT l FROM (SELECT id, place, comments, regdate, numbers) As l)) As properties FROM poi As lg   ) As f )  As fc;";
+const allPois = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(ST_Transform(lg.geom,4326))::json As geometry , row_to_json((SELECT l FROM (SELECT id, place, comments, regdate, numbers, asset_type) As l)) As properties FROM poi As lg   ) As f )  As fc;";
+const getLast = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(ST_Transform(lg.geom,4326))::json As geometry , row_to_json((SELECT l FROM (SELECT id, place, comments, regdate, numbers, asset_type) As l)) As properties FROM poi As lg order by regdate desc limit 1 ) As f )  As fc;";
 
 module.exports = {
   getAll() {
@@ -18,19 +19,57 @@ module.exports = {
   getOne(id) {
     return knex('poi').where('id', id).first();
   },
-
-  //TODO: Make dynamic
-
-  create() {
-    const sql = db.insert({
-      geom: st.geomFromText('Point(-71.064544 44.28787)', 4326)
-    }).into('poi');
-    return sql;
+  getAllwkt() {
+    return db.select('id', st.asText('geom')).from('poi');
   },
+  // get latest
+  getLatest() {
+    return knex('poi').orderBy('regdate', 'asc').limit(1);
+  },
+  getLatestRaw() {
+    // retrun all rows in pois table
+    return knex.raw(getLast);
+  },
+  create(poi) {
+    poi.geom = st.geomFromText(poi.geom, 4326);
+    const sql = db.insert(poi).returning('*').into('poi');
+    console.log(sql.toString());
+    return sql;
+},
   update(id, poi) {
     return knex('poi').where('id', id).update(poi, '*');
   },
   delete(id) {
     return knex('poi').where('id', id).del();
+  },
+  // create new tomming
+  createTomming(tomming) {
+    const sql = db.insert(tomming).returning('*').into('tomming');
+    console.log(sql.toString());
+    return sql;
+        // create tomming with fk value of id
+  },
+  // get tomming for id
+  getTomming(id) {
+    const sql = knex.from('tomming')
+    // .innerJoin('fyllingsgrad', 'regdato', 'poi.id', 'tomming.poi_id')
+    //.innerJoin('*')
+    .where('poi_id', id);
+    console.log(sql.toString());
+    return sql;
+  },
+  createSkade(skade) {
+    const sql = db.insert(skade).returning('*').into('skader');
+    console.log(sql.toString());
+    return sql;
+  },
+  // get skade for id
+  getSkade(id) {
+    const sql = knex.from('skader')
+    // .innerJoin('fyllingsgrad', 'regdato', 'poi.id', 'tomming.poi_id')
+    //.innerJoin('*')
+    .where('poi_id', id);
+    console.log(sql.toString());
+    return sql;
   },
 };
